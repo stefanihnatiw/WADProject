d51d946f0ed34b18e72f1488ff6ce69717b5c800
stefanihnatiw <stefinix997@yahoo.com>
2021-01-20 23:59:31 +0200

    Transitions fix

--- flask-python/static/scripts/open_menu.js
+++ flask-python/static/scripts/open_menu.js
@@ -1,16 +1,29 @@
 var navOpen = false;
 var browseOpen = false;
 
+window.onload = function() {
+  navOpen = (getCookie("navOpen") == "true");
+  if(navOpen) {
+    document.getElementById("mySidenav").style.width = "14%";
+    document.getElementById("image_div").style.setProperty('margin-left', '14%');
+  } else {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("image_div").style.setProperty('margin-left', '0%');
+  }
+};
+
 function updateNav() {
+  document.querySelector('body').classList.remove('preload');
   if(!navOpen) {
     document.getElementById("mySidenav").style.width = "14%";
     document.getElementById("image_div").style.setProperty('margin-left', '14%');
     navOpen = true;
   } else {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("image_div").style.setProperty('margin-left', '7%');
+    document.getElementById("image_div").style.setProperty('margin-left', '0%');
     navOpen = false;
   }
+  setCookie("navOpen", navOpen);
 }
 
 function updateBrowse() {
@@ -21,4 +34,26 @@ function updateBrowse() {
     document.getElementById("searchForm").style.display = "none";
     browseOpen = false;
   }
+}
+
+function setCookie(cname, cvalue) {
+  var d = new Date();
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
